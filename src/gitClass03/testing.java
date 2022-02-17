fa8d9e0fe5b6c9cf9ccc4f078cfd4a5d2641cb5d
Ferdaws-77 <fseraj8@gmail.com>
2022-02-16 19:31:22 -0500

    1st commit

---
+++ src/gitClass03/testing.java
@@ -0,0 +1,12 @@
+package gitClass03;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("water is coloreless!");
+		
+		
+	}
+
+}
