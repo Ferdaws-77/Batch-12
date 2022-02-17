8c346daefaceeea4e9816291558cef876bf6c5b9
Ferdaws-77 <fseraj8@gmail.com>
2022-02-16 20:12:34 -0500

    4 commit

--- src/gitClass03/testing.java
+++ src/gitClass03/testing.java
@@ -5,7 +5,7 @@ public class testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("water is coloreless!");
-	System.out.println("water is blue!");
+	System.out.println("water is blue");
 		
 	}
 
